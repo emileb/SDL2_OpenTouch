d8a1a017f68a834cf0b14c73a409ecb18784cea0
Emile Belanger <emile.belanger@gmail.com>
2021-08-29 16:03:46 +0100

    Change default order to audio devices

--- SDL2-2.0.12/src/audio/SDL_audio.c
+++ SDL2-2.0.12/src/audio/SDL_audio.c
@@ -89,14 +89,14 @@ static const AudioBootStrap *const bootstrap[] = {
 #if SDL_AUDIO_DRIVER_FUSIONSOUND
     &FUSIONSOUND_bootstrap,
 #endif
-#if SDL_AUDIO_DRIVER_AAUDIO
-    &aaudio_bootstrap,
-#endif
 #if SDL_AUDIO_DRIVER_OPENSLES
     &openslES_bootstrap,
 #endif
 #if SDL_AUDIO_DRIVER_ANDROID
     &ANDROIDAUDIO_bootstrap,
+#endif
+#if SDL_AUDIO_DRIVER_AAUDIO
+    &aaudio_bootstrap,
 #endif
 #if SDL_AUDIO_DRIVER_PSP
     &PSPAUDIO_bootstrap,
