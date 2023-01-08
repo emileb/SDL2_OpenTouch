8e5e857d1efdf1f0c53ed78440584dd0a9382af1
Emile Belanger <emile.belanger@gmail.com>
2023-01-08 11:12:41 +0000

    Added fluidsynth option for sound midi mixer

--- Android.mk
+++ Android.mk
@@ -5,6 +5,7 @@ include $(LOCAL_SDL2_PATH)/SDL2-2.0.12/Android.mk
 include $(LOCAL_SDL2_PATH)/SDL2_mixer/Android.mk
 include $(LOCAL_SDL2_PATH)/SDL2_net/Android.mk
 include $(LOCAL_SDL2_PATH)/SDL2_image/Android.mk
+include $(LOCAL_SDL2_PATH)/fluidsynth-lite/src/Android.mk
 
 #SDL_INCLUDE_PATHS = $(LOCAL_SDL2_PATH)/SDL2/include  $(LOCAL_SDL2_PATH)/SDL2_mixer  $(LOCAL_SDL2_PATH)/SDL2_net  $(LOCAL_SDL2_PATH)/SDL2_image
 SDL_INCLUDE_PATHS = $(LOCAL_SDL2_PATH)/SDL2-2.0.12/include  $(LOCAL_SDL2_PATH)/SDL2_mixer  $(LOCAL_SDL2_PATH)/SDL2_net  $(LOCAL_SDL2_PATH)/SDL2_image
